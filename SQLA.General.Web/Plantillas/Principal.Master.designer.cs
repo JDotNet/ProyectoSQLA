17484b92977380ea960cd8de00590e58b041a946
JDotNet <juriol@live.com>
2014-06-21 23:48:59 -0500

    Actualización de Omar

--- SQLA.General.Web/Plantillas/Principal.Master.designer.cs
+++ SQLA.General.Web/Plantillas/Principal.Master.designer.cs
@@ -67,6 +67,15 @@ namespace SQLA.General.Web.Plantillas {
         protected global::System.Web.UI.WebControls.SiteMapDataSource smdsPrincipal;
         
         /// <summary>
+        /// SiteMapPath1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMapPath1;
+        
+        /// <summary>
         /// cphCuerpo control.
         /// </summary>
         /// <remarks>
